4aa73862bee51bde35ad2677da9714a12031fa00
Arun Chandel <97609559+Arun5768@users.noreply.github.com>
2026-08-05 08:14:02 +0000

    Keep the production bundle focused on the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignalLens from "./pages/blank-demo";
-import DesignKitDemo from "./pages/_design";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export default function App() {
@@ -8,7 +7,6 @@ export default function App() {
     <ThemeProvider defaultTheme="dark" storageKey="signal-lens-theme">
       <BrowserRouter>
         <Routes>
-          <Route path="/_design" element={<DesignKitDemo />} />
           <Route path="/" element={<SignalLens />} />
         </Routes>
       </BrowserRouter>
